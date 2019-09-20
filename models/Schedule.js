a17418c6c34bfbbdbb61a7c8f98c01cbe2f306cc
luanperosa <luan.perosa@hotmail.com>
2019-09-19 22:43:20 -0300

    refactor(remove timeOfService)

--- models/Schedule.js
+++ models/Schedule.js
@@ -2,7 +2,6 @@ const { Schema, model } = require('mongoose');
 
 const newSchedule = new Schema({
   dateOfService: { type: Date, required: true },
-  timeOfService: { type: Date, required: true },
   // saloonID: { type: Schema.Types.ObjectId, ref: 'Saloon' },
   // userID: { type: Schema.Types.ObjectId, ref: 'User' },
   // serviceID: { type: Schema.Types.ObjectId, ref: 'Service' },
