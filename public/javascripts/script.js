74092ae366fc789a0c1a47d64255c260ce0a84ff
luanperosa <luan.perosa@hotmail.com>
2019-09-22 14:17:15 -0300

    clean code

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -35,23 +35,3 @@ hourService.onchange = (hour) => {
   
 }
 
-
-// $('.datepicker').pickadate({
-//   monthsFull: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-//   monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-//   weekdaysFull: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabádo'],
-//   weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
-//   today: 'Hoje',
-//   clear: 'Limpar',
-//   close: 'Pronto',
-//   labelMonthNext: 'Próximo mês',
-//   labelMonthPrev: 'Mês anterior',
-//   labelMonthSelect: 'Selecione um mês',
-//   labelYearSelect: 'Selecione um ano',
-//   selectMonths: true, 
-//   selectYears: 15 
-//   });
-
-
-
-
