3572d3f0be1025e33d4c876afeeb35fae25142eb
luanperosa <luan.perosa@hotmail.com>
2019-10-09 13:19:02 -0300

    refactor(front reform)

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -12,6 +12,9 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+
+
+
 // document.addEventListener('DOMContentLoaded', function() {
 //   let elems = document.querySelectorAll('.datepicker');
 //   let instances = M.Datepicker.init(elems, {
